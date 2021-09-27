d58d6bbd7fa6cae61a72e48f04b8c91e20f9e030
Rhythm Bhatia <rhythmbhatiarb28@gmail.com>
2021-09-28 02:01:23 +0530

    basic done

--- src/components/about/achievements.js
+++ src/components/about/achievements.js
@@ -15,8 +15,11 @@ function Achievements() {
           <br />
           <ul>
             <li>
-              Bagged <a href="https://codingcompetitions.withgoogle.com/hashcode/certificate/summary/0000000000435809">346</a> AIR (World Rank: 1835) in Google Hashcode 2021 out of
-              7200+ teams.
+              Bagged{" "}
+              <a href="https://codingcompetitions.withgoogle.com/hashcode/certificate/summary/0000000000435809">
+                346
+              </a>{" "}
+              AIR (World Rank: 1835) in Google Hashcode 2021 out of 7200+ teams.
             </li>
             <li>
               Secured 782 rank in CodeChef Rated Contest Feb 2021 out of 6100+
@@ -25,10 +28,20 @@ function Achievements() {
             <li>
               Top 5% out of 938k people in Linked Skill Assessment of CSS.
             </li>
-            <li>Maximum Rating on CodeChef: <a href="https://www.codechef.com/users/rhythm_28">1709</a> (3 star).</li>
+            <li>
+              Maximum Rating on CodeChef:{" "}
+              <a href="https://www.codechef.com/users/rhythm_28">1709</a> (3
+              star).
+            </li>
             <li>5 stars in Problem Solving on Hacker Rank.</li>
             <li>5 stars in C++ on Hacker Rank.</li>
-            <li>Reached <a href="https://www.hackerrank.com/certificates/3c9aa3ade860">Intermediate</a> Level in C on Hacker Rank.</li>
+            <li>
+              Reached{" "}
+              <a href="https://www.hackerrank.com/certificates/3c9aa3ade860">
+                Intermediate
+              </a>{" "}
+              Level in C on Hacker Rank.
+            </li>
           </ul>
         </div>
         <div className="codingProfiles col-md-5">
@@ -38,17 +51,21 @@ function Achievements() {
             <div>
               <Avatar src={codeChef} sx={{ width: "40px", height: "40px" }} />{" "}
               &nbsp;&nbsp;&nbsp;
-              <h5><a href="https://www.codechef.com/users/rhythm_28">rhythm_28</a></h5>
+              <h5>
+                <a href="https://www.codechef.com/users/rhythm_28">rhythm_28</a>
+              </h5>
             </div>
             <div>
               <Avatar src={hackerRank} sx={{ width: "40px", height: "40px" }} />
               &nbsp;&nbsp;&nbsp;
-              <h5><a href="https://www.hackerrank.com/rhythm_28">rhythm_28</a></h5>
+              <h5>
+                <a href="https://www.hackerrank.com/rhythm_28">rhythm_28</a>
+              </h5>
             </div>
           </div>
           <br />
           <br />
-          <div>
+          <div className="resume">
             <h3>
               My{" "}
               <a href="https://drive.google.com/file/d/1uoskNPYz7vHZDllD7HPryZZa6xY8g6vF/view?usp=sharing">
