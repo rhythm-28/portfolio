8138011790b208aee8580d08dbf6114d0b8cea23
Rhythm Bhatia <rhythmbhatiarb28@gmail.com>
2021-09-27 12:41:29 +0530

    contact me page done

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,14 +1,50 @@
 import react from "react";
+import Avatar from "@mui/material/Avatar";
+
 import "../../stylesheets/contact.css";
+
+import linkedIn from "../../icons/linkedIn.png";
+import github from "../../icons/github.svg";
+import twitter from "../../icons/twitter.svg";
 
 function Contact() {
   return (
     <div>
-      <div className="contact">
-        <h1>Want to hire me?</h1>
-        <h5>E-mail: rhythmbhatia28@gmail.com</h5>
-        <h5></h5>
-        <h5></h5>
+      <div className="contactHead">
+        <h1>Contact Me</h1>
+      </div>
+      <div className="contactDiv container">
+        <div className="row">
+          <div className="col-md-5 contactInfo">
+            <h5>
+              Whether you’re looking to collaborate, talk more about my work or
+              just have a conversation, get in touch.
+            </h5>
+          </div>
+          <div className="col-md-4 emailDiv contactInfo">
+            <h4>Send me an e-mail!</h4>
+            <h5>rhythmbhatia28@gmail.com</h5>
+          </div>
+          <div className="col-md-3 social">
+          <h4>Or connect with me here:</h4>
+            <div>
+              <Avatar src={linkedIn} /> &nbsp;&nbsp;&nbsp;
+              <a href="https://www.linkedin.com/in/rhythm-bhatia/">
+                rhythm-bhatia
+              </a>
+            </div>
+            <div>
+              <Avatar src={github} />
+              &nbsp;&nbsp;&nbsp;
+              <a href="https://github.com/rhythm-28">rhythm-28</a>
+            </div>
+            <div>
+              <Avatar src={twitter} />
+              &nbsp;&nbsp;&nbsp;
+              <a href="https://twitter.com/rhythmb28">rhythmb28</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
