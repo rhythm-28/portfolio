3d76dac2c500b2b0f18644063217420e985e0db6
rhythm-28 <rhythmbhatiarb28@gmail.com>
2022-02-11 11:49:15 +0530

    minor updates

--- src/components/about/achievements.js
+++ src/components/about/achievements.js
@@ -18,16 +18,38 @@ function Achievements(props) {
   return (
     <div>
       <div className="container achievements">
-        <div data-aos={viewportWidth > 696 ? "fade-right" : "fade-up"} className="col-md-7">
+        <div
+          data-aos={viewportWidth > 696 ? "fade-right" : "fade-up"}
+          className="col-md-7"
+        >
           <h3 className="achievementsHead">ACHIEVEMENTS</h3>
           <br />
           <ul>
             <li>
               Bagged{" "}
-              <a href="https://codingcompetitions.withgoogle.com/hashcode/certificate/summary/0000000000435809">
+              <a href="https://drive.google.com/file/d/15AfqhB08tDYfII7Itksti8pTg-PQl0Xj/view?usp=sharing">
                 346
               </a>{" "}
               AIR (World Rank: 1835) in Google Hashcode 2021 out of 7200+ teams.
+            </li>
+            <li>
+              4 stars on CodeChef with a maximum Rating of{" "}
+              <a href="https://www.codechef.com/users/rhythm_28">1808</a> (3
+              star).
+            </li>
+            <li>
+              Secured{" "}
+              <a href=" https://drive.google.com/file/d/1QP2y6NwIBT8D7BVXxz-LVKtvN7AW6GvP/view?usp=sharing">
+                2148
+              </a>{" "}
+              rank in Google Kickstart Round H 2021.
+            </li>
+            <li>
+              Secured{" "}
+              <a href=" https://drive.google.com/file/d/1QP2y6NwIBT8D7BVXxz-LVKtvN7AW6GvP/view?usp=sharing">
+                2305
+              </a>{" "}
+              rank in Google Kickstart Round G 2021.
             </li>
             <li>
               Secured 782 rank in CodeChef Rated Contest Feb 2021 out of 6100+
@@ -36,13 +58,7 @@ function Achievements(props) {
             <li>
               Top 5% out of 938k people in Linked Skill Assessment of CSS.
             </li>
-            <li>
-              Maximum Rating on CodeChef:{" "}
-              <a href="https://www.codechef.com/users/rhythm_28">1709</a> (3
-              star).
-            </li>
-            <li>5 stars in Problem Solving on Hacker Rank.</li>
-            <li>5 stars in C++ on Hacker Rank.</li>
+            <li>5 stars in Problem Solving, C++ on Hacker Rank.</li>
             <li>
               Reached{" "}
               <a href="https://www.hackerrank.com/certificates/3c9aa3ade860">
@@ -52,7 +68,10 @@ function Achievements(props) {
             </li>
           </ul>
         </div>
-        <div data-aos={viewportWidth > 696 ? "fade-left" : "fade-up"} className="codingProfiles col-md-5">
+        <div
+          data-aos={viewportWidth > 696 ? "fade-left" : "fade-up"}
+          className="codingProfiles col-md-5"
+        >
           <h3 className="codingProfilesHead">Coding profiles</h3>
           <br />
           <div className="profiles">
@@ -76,7 +95,7 @@ function Achievements(props) {
           <div className="resume">
             <h4>
               Here is my{" "}
-              <a href="https://drive.google.com/file/d/1Pq3DoI0BszOYS5KdL_RMU7EHkgoKCftG/view?usp=sharing">
+              <a href="https://drive.google.com/file/d/1-qEOF3GlkDrAC5hgOvtj96iwhkZN7aOk/view?usp=sharing">
                 Resume
               </a>
               &nbsp;!
